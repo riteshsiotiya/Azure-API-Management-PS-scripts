e8483ff6251e71b64e68f150b62f4a9e07c5a7e9
riteshsiotiya <riteshsiotiya007@gmail.com>
2021-05-17 12:27:31 +0530

    Add files via upload

---
+++ ApimDeployment.ps1
@@ -0,0 +1,77 @@
+﻿param(
+    [string]$ScriptPath,
+    [string]$ApimJsonFileName,
+    [string]$ApimPolicyFileName,
+    [string]$AzureResourceGroupName,
+    [string]$AzureServiceName,
+    [string]$ApimApiPath,
+    [string]$ApimApiName,
+    [string]$ApimApiServiceUrl,
+    [string[]]$Policy_OperationNamesList,
+    [string]$Environment,
+    [string]$ProductId
+
+)
+
+Write-Host "=========================================================================
+          Azure Api Management service creation or updation 
+=========================================================================" -ForegroundColor Green 
+
+Get-AzSubscription
+
+Write-Host "APIM service deployment started.."
+
+$context = New-AzApiManagementContext -ResourceGroupName $AzureResourceGroupName -ServiceName $AzureServiceName
+
+Write-Host "Importing APIM JSON file"
+
+$ApimJsonFilePath="$ScriptPath\APIs\$ApimJsonFileName"
+$ApimPolicyFilePath="$ScriptPath\Policy\API_Policy\$ApimPolicyFileName"
+
+Write-Host "Apim Json File Path $ApimJsonFilePath"
+Write-Host "Apim Policy File path $ApimPolicyFilePath"
+
+
+Import-AzApiManagementApi -Context $context -SpecificationPath "$ApimJsonFilePath" -SpecificationFormat "OpenApi" -Path $ApimApiPath -ApiId $ApimApiName -ServiceUrl $ApimApiServiceUrl
+
+Write-Host "Imported APIM JSON file successfully" -ForegroundColor Green 
+
+
+if (-not ([string]::IsNullOrEmpty($ApimPolicyFileName)))
+{
+    Write-Host "Applying policy : $ApimApiName"
+    Set-AzApiManagementPolicy -Context $context -PolicyFilePath  "$ApimPolicyFilePath" -ApiId $ApimApiName
+    Write-Host "Policy applied successfully." -ForegroundColor Green 
+}
+else
+{
+    Write-Host "Skipping policy, value of ApplyPocicy found  : $ApplyPocicy" -ForegroundColor Yellow
+}
+
+if ($Policy_OperationNamesList -ne 0)
+{
+    Write-Host "operationFilePath $operationFilePath"
+    Write-Host "Setting up operation level policy .."
+    foreach ($operationName in $Policy_OperationNamesList)
+    {
+        $operationFilePath="$ScriptPath\Policy\Operation_Policy\$operationName`_$Environment.xml"
+        Set-AzApiManagementPolicy -Context $context -PolicyFilePath "$operationFilePath" -ApiId $ApimApiName -OperationId $operationName
+    }
+}
+else
+{
+    Write-Host "Skipping operation level policy, count of Policy_OperationNamesList found 0" -ForegroundColor Yellow
+}
+
+Write-Host "Mapping existing product to APIM, Product id  $ProductId"
+Add-AzApiManagementApiToProduct -Context $context -ProductId $ProductId -ApiId $ApimApiName
+
+Write-Host "Product mapping completed successfully" -ForegroundColor Green
+
+
+#$PropertiesObject = @{
+#    displayName = "Grand Central"
+#}
+#New-AzResource -PropertyObject $PropertiesObject -ResourceGroupName $AzureResourceGroupName -ResourceType Microsoft.ApiManagement/service/tags -ResourceName "<API-Management-service-name>/$ResourceName" -ApiVersion 2018-01-01 -Force
+
+#Set-AzResource -PropertyObject $PropertiesObject  -ResourceType Microsoft.ApiManagement/service/tags -ResourceGroupName $AzureResourceGroupName -ResourceName $AzureServiceName/$ApimApiName -Force
---
+++ ApimOAuthDeployment.ps1
@@ -0,0 +1,41 @@
+﻿
+
+param(
+    [string]$AzureResourceGroupName,
+    [string]$AzureServiceName,
+    [string]$OAuthServerId,
+    [string]$OAuthServerName,
+    [string]$ClientRegistrationPageUrl,
+    [string]$AuthorizationEndpointUrl,
+    [string]$TokenEndpointUrl,
+    [string]$ClientId,
+    [string]$ClientSecret,
+    [string[]]$AuthorizationRequestMethods,
+    [string[]]$GrantTypes,
+    [string[]]$ClientAuthenticationMethods,
+    [hashtable]$TokenBodyParameters,
+    [string[]]$AccessTokenSendingMethods
+)
+
+Write-Host "=========================================================================
+           Azure Api Management service OAuth2.0 server creation 
+=========================================================================" -ForegroundColor Green 
+Get-AzSubscription
+
+Write-Host "Verifying if requested OAuth server is alredy exist or not..."
+
+$ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName $AzureResourceGroupName -ServiceName $AzureServiceName
+$getApiMgtOAuthServer = Get-AzApiManagementAuthorizationServer -Context $ApiMgmtContext -ServerId $OAuthServerId
+
+if($getApiMgtOAuthServer)
+{
+    Write-Host "APIM OAuth2.0 server: $OAuthServerId is already exist" -ForegroundColor Yellow
+}
+else
+{
+    Write-Host "APIM OAuth2.0 server: $OAuthServerId does not exist" -ForegroundColor Green
+    Write-Host "Azure Api Management service OAuth2.0 server creation started..."
+    New-AzApiManagementAuthorizationServer -Context $ApiMgmtContext -ServerId $OAuthServerId -Name $OAuthServerName -ClientRegistrationPageUrl $ClientRegistrationPageUrl -AuthorizationEndpointUrl $AuthorizationEndpointUrl -TokenEndpointUrl $TokenEndpointUrl -ClientId $ClientId -ClientSecret $ClientSecret -AuthorizationRequestMethods @($AuthorizationRequestMethods) -GrantTypes @($GrantTypes) -ClientAuthenticationMethods @($ClientAuthenticationMethods) -TokenBodyParameters $TokenBodyParameters -AccessTokenSendingMethods @($AccessTokenSendingMethods)
+}
+
+#New-AzApiManagementAuthorizationServer -Context $context -Name "OAuth2-server-pipeline" -ClientRegistrationPageUrl "https://localhost" -AuthorizationEndpointUrl "https://login.microsoftonline.com/582259a1-dcaa-4cca-b1cf-e60d3f045ecd/oauth2/authorize" -TokenEndpointUrl "https://login.microsoftonline.com/582259a1-dcaa-4cca-b1cf-e60d3f045ecd/oauth2/token" -ClientId "f2db16a5-08b7-4b96-94c2-207dcbaabe0c" -ClientSecret "9IV5?6XYiH9msCT-QHxgNfvVQjzNsY_[" -AuthorizationRequestMethods @('Get', 'Post') -GrantTypes AuthorizationCode -ClientAuthenticationMethods Body -TokenBodyParameters @{'Resource'='f2db16a5-08b7-4b96-94c2-207dcbaabe0c'} -AccessTokenSendingMethods AuthorizationHeader
